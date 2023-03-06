214b9fbac7f6af60f9ad2ac696248b4faf5d3527
Vladislav <eyeseeices@gmail.com>
2023-03-06 17:46:27 +0300

    remove redirect

--- src/index.tsx
+++ src/index.tsx
@@ -17,9 +17,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
